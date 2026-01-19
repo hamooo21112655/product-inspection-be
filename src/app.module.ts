43c41832e14b2453df066a6f4edf639d87395573
Hamo <hamooo21112655@gmail.com>
2026-01-19 20:31:26 +0100

    switched to postgres for deployment purposes

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,14 @@ import { InspectionsModule } from './inspections/inspections.module';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
       autoLoadEntities: true,
       synchronize: true,
     }),
