3a611e8cc877994067692f92676bb197a500a28c
Hamo <hamooo21112655@gmail.com>
2026-01-19 20:57:38 +0100

    enabled all origins

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:8080',
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
